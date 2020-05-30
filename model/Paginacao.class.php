409be00f85814fe0121944fa9a9a0ab424311900
Alvaro Lima <alvarolima1103@outlook.com>
2020-05-30 14:06:28 -0300

    Configurado o objeto 'Paginacao' para que ele possa fazer a consulta dos itens no banco

--- model/Paginacao.class.php
+++ model/Paginacao.class.php
@@ -6,21 +6,36 @@ class Paginacao extends Conexao {
 
 
     function GetPaginacao($campo, $tabela) {
-        $query = "SELECT {$campo} FROM {$tabala}";
+        $query = "SELECT {$campo} FROM {$tabela}";
         $this->ExecuteSQL($query);
         $total = $this->TotalDados();
 
         $this->limite = Config::DB_LIMIT_POR_PAG;
-        $paginas = ceil($total / $this->$limite); //ceil arredonda pra cima
+        $paginas = ceil($total / $this->limite); //ceil arredonda pra cima
         $this->totalpags = $paginas;
 
         //verificacao com Operador Ternario
         $p = (int)isset($_GET['p']) ? $_GET['p'] : 1; 
 
+        //Este if direcional para a ultima pagina existente
+        if($p > $paginas) {
+            $p = $paginas;
+        }
+
         //Testar na pratica
         $this->inicio = ($p * $this->limite) - $this->limite;
 
-        for($i = 1; $i <= $paginas; $i++):
+        //Limite para mostar o numero de paginas 
+        $tolerancia = 4;
+        $mostrar = $p + $tolerancia;
+        if($mostrar > $paginas) {
+            $mostrar = $paginas;
+        }
+
+        for($i = ($p - $tolerancia); $i <= $mostrar; $i++):
+            if($i < 1) {
+                $i = 1;
+            }
             array_push($this->link, $i);
 
         endfor;
